e9548c5c6a417baf44fc7797daf91af845eb5e56
Amirhossein Moghadam <Amirhoussein.Moghadam79@gmail.com>
2021-08-09 02:48:20 +0430

    demo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { EnzymeAdapter } from "enzyme";
+import React from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Rendering Correct", () => {
+
 });
+
+// import { render, screen } from '@testing-library/react';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
