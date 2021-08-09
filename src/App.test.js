5a9b41f303d89120e12ea4cfaaa822c9d047bb77
AmirHossein Moghaddam <moghaddam@chargoon.net>
2021-08-09 09:29:47 +0430

    Dependancy Ok

--- src/App.test.js
+++ src/App.test.js
@@ -6,10 +6,4 @@ test("Rendering Correct", () => {
 
 });
 
-// import { render, screen } from '@testing-library/react';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
